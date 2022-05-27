aaa1c504ccc2cc1d1229c88b1c5a5e50c60dfe09
Daniel Miranda <danielmesa877@gmail.com>
2022-05-27 17:08:41 -0500

    handling headers

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express');
-const router = express.Router(); 
-const bodyParser = require('body-parser');
+//const router = express.Router();  ** [DEPRECATED]
+//const bodyParser = require('body-parser'); ** [DEPRECATED]
 
 
 var app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(router);
+//app.use(bodyParser.json()); ** [DEPRECATED]
+app.use(express.json());
+//app.use(bodyParser.urlencoded({extended: false})); ** [DEPRECATED]
+app.use(express.urlencoded({extended:false}));
+//app.use(router); ** [DEPRECATED]
 
-router.get('/message', (req, res) => {
+app.get('/message', (req, res) => {
+    console.log(req.headers); //receibe headers
+
+    res.header({
+        "custom-headers": "Nuestro Header"
+    }); //send custom headers
+
     res.send('Lista de mensajes');
 });
 
-router.post('/message', (req, res) => {
+app.post('/message', (req, res) => {
     console.log(req.query);
     console.log(req.body);
     res.send('Mensaje añadido');
